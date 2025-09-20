e5655f0a8abed02c1ac11b6ca36f9530a4c04b05
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2025-09-20 09:34:28 -0500

    feat: agregar nuevas rutas para secciones de requisitos, procesos y costos en la página

--- routes/web.php
+++ routes/web.php
@@ -42,9 +42,15 @@ Route::get('/buscar/record-academico/{admitido}', [RecordAcademicoController::cl
 // Ruta para la pagina de inicio
 Route::get('/', [PaginaController::class, 'inicio'])->name('pagina.inicio');
 Route::get('/noticia/{slug}', [PaginaController::class, 'noticia'])->name('pagina.noticia');
+
 Route::get('/mision', [PaginaController::class, 'mision'])->name('pagina.mision');
 Route::get('/vision', [PaginaController::class, 'vision'])->name('pagina.vision');
 Route::get('/objetivos', [PaginaController::class, 'objetivos'])->name('pagina.objetivos');
 Route::get('/resena-historica', [PaginaController::class, 'resena_historica'])->name('pagina.resena-historica');
 Route::get('/autoridades', [PaginaController::class, 'autoridades'])->name('pagina.autoridades');
 Route::get('/reglamento', [PaginaController::class, 'reglamento'])->name('pagina.reglamento');
+
+Route::get('/requisito-ingreso', [PaginaController::class, 'requisito_ingreso'])->name('pagina.requisito-ingreso');
+Route::get('/procesos-cronogramas', [PaginaController::class, 'procesos_cronogramas'])->name('pagina.procesos-cronogramas');
+Route::get('/costos-modalidades', [PaginaController::class, 'costos_modalidades'])->name('pagina.costos-modalidades');
+Route::get('/link-siepg', [PaginaController::class, 'link_siepg'])->name('pagina.link-siepg');
